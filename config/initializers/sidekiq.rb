a320a2107dc136e67985ca0659f3d95e146fc88d
codefactor-io <support@codefactor.io>
2018-10-07 10:10:53 +0000

    [CodeFactor] Apply fixes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,7 @@
 namespace    = ENV.fetch('REDIS_NAMESPACE') { nil }
 redis_params = { url: ENV['REDIS_URL'] }
 
-if namespace
-  redis_params[:namespace] = namespace
-end
+redis_params[:namespace] = namespace if namespace
 
 Sidekiq.configure_server do |config|
   config.redis = redis_params
